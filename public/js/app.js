4443942e542d4f30ac9012237f7e67010bf971c2
Mo2024 <mohdosama2025@gmail.com>
2022-08-05 23:27:38 +0300

    new modifications

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+function resizeInput() {
+    $(this).attr('size', $(this).val().length);
+}
+
+$('input[type="text"]')
+    // event handler
+    .keyup(resizeInput)
+    // resize on page load
+    .each(resizeInput);
